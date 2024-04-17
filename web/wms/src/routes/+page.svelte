5ff40f939aaf03c27f5738edbcc32d07eda8a693
RensVanGiersbergen <rensgiersbergen@gmail.com>
2024-04-17 11:12:01 +0200

    Added titles and favicon

--- web/wms/src/routes/+page.svelte
+++ web/wms/src/routes/+page.svelte
@@ -1,1 +1,5 @@
+<svelte:head>
+	<title>WMS</title>
+</svelte:head>
+
 <h1 class="text-slate-700">Welcome to Wallmarkt WMS</h1>
--- web/wms/src/routes/stock/+page.svelte
+++ web/wms/src/routes/stock/+page.svelte
@@ -1,1 +1,5 @@
+<svelte:head>
+	<title>WMS - Stock</title>
+</svelte:head>
+
 <h1>Stock</h1>
