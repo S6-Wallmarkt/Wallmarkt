947c47ffdd8d191295eb345e671ce40624bc2c2e
RensVanGiersbergen <rensgiersbergen@gmail.com>
2024-05-29 08:49:26 +0200

    wrote first e2e login test

--- web/wms/tests/test.ts
+++ web/wms/tests/test.ts
@@ -1,6 +1,20 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+test('User can log in using Auth0 SPA SDK and see menu', async ({ page }) => {
+	await page.goto('http://localhost:5173/');
+	await page.waitForTimeout(1000);
+	const page1Promise = page.waitForEvent('popup');
+	await page.getByRole('button', { name: 'Login' }).click();
+	const page1 = await page1Promise;
+	await page1.getByLabel('Email address*').click();
+	await page1.getByLabel('Password*').click();
+	await page1.getByLabel('Password*').fill('xjUZ_AA4Aqd.KDW');
+	// await page1.getByLabel('Password*').fill(process.env.PLAYWRIGHT_PASSWORD);
+	await page1.getByLabel('Email address*').click();
+	await page1.getByLabel('Email address*').fill('yaseg80297@qiradio.com');
+	// await page1.getByLabel('Email address*').fill(process.env.PLAYWRIGHT_EMAIL);
+	await page1.getByRole('button', { name: 'Continue', exact: true }).click();
+	await page.waitForTimeout(5000);
+	await expect(await page.getByRole('img', { name: 'User Avatar' })).toBeEnabled();
+	await expect(await page.getByRole('link', { name: 'Home' })).toBeEnabled();
 });
