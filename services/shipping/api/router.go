0c167f9ffaf4ea0ac6f44c22ba229aaddfe05847
RensVanGiersbergen <rensgiersbergen@gmail.com>
2024-05-31 21:18:06 +0200

    added crud

--- services/shipping/api/router.go
+++ services/shipping/api/router.go
@@ -1,12 +1,92 @@
 package api
 
 import (
+	"net/http"
+
+	shippingLogic "github.com/S6-Wallmarkt/Wallmarkt/services/shipping/internal"
+	"github.com/S6-Wallmarkt/Wallmarkt/services/shipping/models"
 	"github.com/gin-gonic/gin"
 )
+
+const internalServerErrorMessage string = "Something went wrong, try again later"
 
 // Function to setup the router and its routes
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
+	// POST: Create Shipment
+	router.POST("/add", func(c *gin.Context) {
+		var shipment models.Shipment
+
+		// Bind the JSON data from the request body to the shipment struct
+		if err := c.ShouldBindJSON(&shipment); err != nil {
+			// Handle the case where JSON binding fails
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+			return
+		}
+
+		id, err := shippingLogic.Add(shipment)
+
+		if err != nil {
+			// Return internal error if failed
+			c.JSON(http.StatusBadRequest, gin.H{"error": internalServerErrorMessage})
+			return
+		}
+
+		// Return the created product
+		c.JSON(http.StatusCreated, id)
+	})
+
+	// GET: Get shipment by ID
+	router.GET("/getbyid/:id", func(c *gin.Context) {
+		shipmentID := c.Param("id")
+
+		shipment, err := shippingLogic.GetByID(shipmentID)
+		if err != nil {
+			if err.Error() == "shipment not found" {
+				c.JSON(http.StatusNotFound, gin.H{"error": "shipment not found"})
+				return
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": internalServerErrorMessage})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, shipment)
+	})
+
+	// GET: Get all shipments
+	router.GET("/getall", func(c *gin.Context) {
+		shipments, err := shippingLogic.GetAll()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": internalServerErrorMessage})
+			return
+		}
+
+		// If no orders found
+		if len(shipments) == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "No shipments found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, shipments)
+	})
+
+	// GET: Get all unsend shipments
+	router.GET("/getallunsend", func(c *gin.Context) {
+		shipments, err := shippingLogic.GetAllUnsend()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": internalServerErrorMessage})
+			return
+		}
+
+		// If no orders found
+		if len(shipments) == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "No shipments found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, shipments)
+	})
 	return router
 }
--- services/shipping/internal/shipping.go
+++ services/shipping/internal/shipping.go
@@ -1,1 +1,44 @@
-package shipping
+package internal
+
+import (
+	"github.com/S6-Wallmarkt/Wallmarkt/services/shipping/models"
+	"github.com/S6-Wallmarkt/Wallmarkt/services/shipping/repository"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var ShipmentRepository repository.ShipmentRepository
+
+func Add(shipment models.Shipment) (string, error) {
+	id, err := ShipmentRepository.AddShipment(shipment)
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+	return id, nil
+}
+
+func GetByID(shipmentID string) (models.Shipment, error) {
+	shipment, err := ShipmentRepository.GetShipmentByID(shipmentID)
+	if err != nil {
+		log.Error(err)
+		return models.Shipment{}, err
+	}
+	return shipment, nil
+}
+
+func GetAll() ([]models.Shipment, error) {
+	shipments, err := ShipmentRepository.GetAllShipments()
+	if err != nil {
+		return []models.Shipment{}, err
+	}
+	return shipments, nil
+}
+
+func GetAllUnsend() ([]models.Shipment, error) {
+	shipments, err := ShipmentRepository.GetAllUnsendShipments()
+	if err != nil {
+		return []models.Shipment{}, err
+	}
+	return shipments, nil
+}
